4fd09a7e18bd82948c5f3c007e37fcb3546e7ef9
parniyan <mousaieparniyan@gmail.com>
2023-07-12 12:29:41 +0330

    change manifest of dot loading

--- app/src/main/java/com/parniyan/dotloading/MainActivity.kt
+++ app/src/main/java/com/parniyan/dotloading/MainActivity.kt
@@ -2,10 +2,13 @@ package com.parniyan.dotloading
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.parniyan.parniyandotloading.LoadingView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val loadingView = findViewById<LoadingView>(R.id.loadingView)
+        loadingView.startLoading()
     }
 }
